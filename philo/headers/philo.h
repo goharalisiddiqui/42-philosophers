1c30a8ad3b5be4b3751ad0435ab4e72cab396c2a
Gohar ali Siddiqui <gsiddiqu@2-H-3.42heilbronn.de>
2021-09-29 22:18:35 +0200

    header normed

--- philo/headers/philo.h
+++ philo/headers/philo.h
@@ -6,7 +6,7 @@
 /*   By: gsiddiqu <gsiddiqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 13:22:57 by edavid            #+#    #+#             */
-/*   Updated: 2021/09/29 21:04:51 by gsiddiqu         ###   ########.fr       */
+/*   Updated: 2021/09/29 22:18:09 by gsiddiqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,53 +66,34 @@ typedef struct s_menu
 }	t_menu;
 
 // Function declarations
-
-// ph_main.c
 pthread_mutex_t	*make_fork(void);
-
-
-// threads.c
-void	*philosopher(void *menu);
-t_menu	*prepare_menu(t_philo data, int i, pthread_mutex_t *pm, t_list *ml);
-int		make_philosophers(t_list **ml, t_philo data, pthread_mutex_t *pm);
-void	monitor_philosophers(t_list *menulist, t_philo data);
-
-
-// ph_argcheck.c
-int		ph_argcheck(int argc, char **argv, t_philo *data);
-
-// ph_utils1.c
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-int		ft_isnum(char *str);
-int		ft_strlen(char *str);
-void	ft_putnbr_fd(long n, int fd);
-
-// time_funcs.c
-void	myusleep(long usec);
-int		check_pulse(long lasteat, long die);
-int		wake_philo(t_menu *menu);
-int		get_currtime(long *time);
-void	mysleep(long msec);
-
-// ft_atoil.c
-long	ft_atol(char *str);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-
-// list_utils.c
-t_list	*ft_lstnew(void *content);
-void	ft_lstadd_back(t_list **lst, t_list *new);
-void	ft_lstclear(t_list **lst, void (*del)(void*));
-t_list	*ft_lstlast(t_list *lst);
-int 	ft_err(char *str);
-
-// display_message.c
-void	display_message(t_menu *menu, int type);
-
-// destry_funcs.c
-void	destroy_data(t_philo data, t_list **menulist);
-void	destroy_menu(t_menu *menu);
-
+void			*philosopher(void *menu);
+t_menu			*prepare_menu(t_philo data, int i,
+					pthread_mutex_t *pm, t_list *ml);
+int				make_philosophers(t_list **ml, t_philo data,
+					pthread_mutex_t *pm);
+void			monitor_philosophers(t_list *menulist, t_philo data);
+int				ph_argcheck(int argc, char **argv, t_philo *data);
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
+int				ft_isnum(char *str);
+int				ft_strlen(char *str);
+void			ft_putnbr_fd(long n, int fd);
+void			myusleep(long usec);
+int				check_pulse(long lasteat, long die);
+int				wake_philo(t_menu *menu);
+int				get_currtime(long *time);
+void			mysleep(long msec);
+long			ft_atol(char *str);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
+t_list			*ft_lstnew(void *content);
+void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstclear(t_list **lst, void (*del)(void*));
+t_list			*ft_lstlast(t_list *lst);
+int				ft_err(char *str);
+void			display_message(t_menu *menu, int type);
+void			destroy_data(t_philo data, t_list **menulist);
+void			destroy_menu(t_menu *menu);
 
 #endif
